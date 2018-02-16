0766674bf4068194bfd56524ed62a0ca19cef0ae
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-02-16 09:54:38 -0500

    checkHumberId function added

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -1,7 +1,14 @@
 //LAB 3 - UNIT TESTING
-alert("lab 3");//please delete this line once connected.
 /**
  * Validate input is a Humber College Student number.
  * Returns true if input validates.
  * @param {string} idIn
  */
+function checkHumberId(idIn) {
+	let result = false
+	const pattern = /n\d{8}$/
+	result=pattern.test(idIn)
+	return result
+}
+
+
