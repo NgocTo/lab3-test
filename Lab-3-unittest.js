3c32f72a6d64df7917555d51cdcea7913170ebbf
NgocTo <31940774+NgocTo@users.noreply.github.com>
2018-02-16 09:57:49 -0500

    added test function

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -11,4 +11,29 @@ function checkHumberId(idIn) {
 	return result
 }
 
+function test__checkHumbrId (num, expected) {
+    // "use strict";
+    var announcedResult;
+    // validate humber id
+    var result = checkHumberId(num);
+    
+    // check if the expected result is the same with result
+    if (result == expected) {
+        announcedResult = "==PASSED==";
+    } else {
+        announcedResult = "xxFAILEDxx";
+    }
+    
+    // output message
+    var msg = "Value tested: " + num + "Expected result: " + expected + "<br/>";
+    var dt = document.getElementById("data");
+    dt += msg;
+}
+// test values
+test__checkHumbrId ("N12345678", true);
+test__checkHumbrId ("001256331", false);
+test__checkHumbrId ("n12345678", true);
+test__checkHumbrId ("001256331", false);
+test__checkHumbrId ("ABCDEFGHI", false);
+test__checkHumbrId ("n1234567", false);
 
