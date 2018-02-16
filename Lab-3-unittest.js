4dd3aad5756d7c034a2e9a776473174626d7a522
Sviatoslav Tokariev <svtokarevsv@gmail.com>
2018-02-16 10:22:52 -0500

    jslint fixes

--- Lab-3-unittest.js
+++ Lab-3-unittest.js
@@ -5,35 +5,32 @@
  * @param {string} idIn
  */
 function checkHumberId(idIn) {
-	let result = false
-	const pattern = /n\d{8}$/i
-	result=pattern.test(idIn)
-	return result
-}
-
-function test__checkHumbrId (num, expected) {
     "use strict";
-    var announcedResult;
+    let result = false;
+    const pattern = /n\d{8}$/i;
+    result = pattern.test(idIn);
+    return result;
+}
+function test__checkHumbrId(num, expected) {
+    "use strict";
+    let announcedResult;
     // validate humber id
-    var result = checkHumberId(num);
-    
+    const result = checkHumberId(num);
     // check if the expected result is the same with result
     if (result === expected) {
         announcedResult = "<span class='success'>==PASSED==</span>";
     } else {
         announcedResult = "<span class='error'>xxFAILEDxx</span>";
     }
-    
     // output message
-    var msg = "Value tested: " + num + " Expected result: " + expected + " " + announcedResult + "<br/>";
-    var dt = document.getElementById("data");
+    let msg = "Value tested: " + num + " Expected result: " + expected + " " + announcedResult + "<br/>";
+    const dt = document.getElementById("data");
     dt.innerHTML += msg;
 }
 // test values
-test__checkHumbrId ("N12345678", true);
-test__checkHumbrId ("001256331", false);
-test__checkHumbrId ("n12345678", true);
-test__checkHumbrId ("001256331", false);
-test__checkHumbrId ("ABCDEFGHI", false);
-test__checkHumbrId ("n1234567", false);
-
+test__checkHumbrId("N12345678", true);
+test__checkHumbrId("001256331", false);
+test__checkHumbrId("n12345678", true);
+test__checkHumbrId("001256331", false);
+test__checkHumbrId("ABCDEFGHI", false);
+test__checkHumbrId("n1234567", false);
